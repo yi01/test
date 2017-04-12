715aa4140667f6ff014270d5906fc296ae0b2046
生澤祐平 <ikuzawayuhei@ikuzawayuutaira-no-MacBook-Air.local>
2017-04-12 14:15:03 +0900

    fc

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
+    collection do
+      post :confirm
+    end
+  end
+
+  resources :contacts, only: [:new, :create] do
+    collection do
+      post :confirm
+    end
+  end
+  root 'blogs#index'
+end
